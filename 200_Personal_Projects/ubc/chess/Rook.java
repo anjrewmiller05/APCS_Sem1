d90c9bbb8c206b973e8ca99394031593f5ead0b7
anjrewmiller05 <andrewdmiller2009@gmail.com>
2026-02-23 20:12:20 -0800

    fixed all pieces landing on each other (same color)

--- 200_Personal_Projects/ubc/chess/Rook.java
+++ 200_Personal_Projects/ubc/chess/Rook.java
@@ -3,21 +3,58 @@ package chess;
 public class Rook extends Piece {
     
     public Rook(int id, boolean white, int row, int col) {
-        super("Rook", id, white, row, col);
+        super ("Rook", id, white, row, col);
     }
     
     public boolean isValidMove(Board board, int row, int col) {
-        if (row > 7 || row < 0 || col < 0 || col > 7) { // if its off the board
+        if (row > 7 || row < 0 || col < 0 || col > 7) { // if it's off the board
              return false;   
         }
 
+        if (getColumn() != col && getRow() != row) {
+            return false;
+        }
 
         Piece blocker = board.whosThere(row, col);
-        if(blocker != null) {
+        if (blocker != null) {
             if ((isWhite() && blocker.isWhite()) || (!isWhite() && !blocker.isWhite())) {
                 return false;
             }
         }
+
+        if (getRow() < row) {
+            for (int i = row - 1; i > getRow(); i--) {
+                Piece blocker2 = board.whosThere(i, col);
+                if(blocker2 != null) {
+                    return false;
+                }
+            }
+        }
+        else if (getRow() > row) {
+            for (int i = row + 1; i < getRow(); i++){
+                Piece blocker2 = board.whosThere(i, col);
+                if(blocker2 != null) {
+                    return false;
+                }
+            }
+        }
+        else if (getColumn() < col) {
+            for (int i = col - 1; i > getColumn(); i--) {
+                Piece blocker2 = board.whosThere(row, i);
+                if (blocker2 != null) {
+                    return false;
+                }
+            }
+        }
+        else {
+            for (int i = col + 1; i < getColumn(); i++) {
+                Piece blocker2 = board.whosThere(row, i);
+                if (blocker2 != null) {
+                    return false;
+                }
+            }
+        }
+
         return true;
     }
 
